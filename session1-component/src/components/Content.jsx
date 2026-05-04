c3739b6cbaf97f1fa2898e703d3ec444769077f0
DuyPhuong <15520659@gm.uit.edu.vn>
2026-05-04 19:08:23 +0700

    Init git

---
+++ session1-component/src/components/Content.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Content = () => {
+  
+  return (
+    <div className='bg-blue-300 flex justify-center item-center py-5 text-3xl'>
+      <h1>Content</h1>
+    </div>
+  )
+}
+
+export default Content
